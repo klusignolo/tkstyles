93cc92a43ddd1a1749c38571cfa6063b8a640318
Pegasus\kevin <kevinlusignolo@gmail.com>
2020-12-08 12:31:27 -0600

    no message

--- Setup.py
+++ Setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Kevin Lusignolo',                   # Type in your name
   author_email='kevinlusignolo@gmail.com',      # Type in your E-Mail
   url='https://github.com/klusignolo/tkstyles',   # Provide either the link to your github or to your website
-  download_url='https://github.com/klusignolo/tkstyles/archive/2.1.4.tar.gz',    # I explain this later on
+  download_url='https://github.com/klusignolo/tkstyles/archive/v2.1.4.tar.gz',    # I explain this later on
   keywords=['tkinter', 'style', 'theme'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
